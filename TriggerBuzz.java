efd742bd7ba360ef96c42bdb6c294cd0be7a61f6
Mike <46755443+msers@users.noreply.github.com>
2019-04-17 11:37:18 -0400

    Update and rename Start.java to TriggerBuzz.java

---
+++ TriggerBuzz.java
@@ -0,0 +1,45 @@
+/*
+
+
+*  ControllerTester
+
+
+*/
+package com.studiohartman.jamepad.tester;
+
+import com.studiohartman.jamepad.ControllerIndex;
+import com.studiohartman.jamepad.ControllerManager;
+import com.studiohartman.jamepad.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class TriggerBuzz
+{
+   public static int NUM_CONTROLLERS = 2;
+
+   public static void main(String args[])
+   {
+      ControllerManager controllers = new ControllerManager(NUM_CONTROLLERS);
+      controllers.initSDLGamepad();
+      
+      while(true) 
+      {
+         ControllerState currState = controllers.getState(0);
+  
+         if(!currState.isConnected || currState.b) 
+         {
+         
+            System.out.println("Seeya Nerd");
+            break;
+         }
+         if(currState.a) 
+         {
+            System.out.println("\"A\" on \"" + currState.controllerType + "\" is pressed");
+         }
+      }
+      
+   }
+          
+}
